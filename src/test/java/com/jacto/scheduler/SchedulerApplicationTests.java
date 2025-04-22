edc5986ad7ebf6f5f5ad62e140e104eb82e3f5dd
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2025-04-22 16:28:25 -0300

    Initial structure

---
+++ src/test/java/com/jacto/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacto.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
